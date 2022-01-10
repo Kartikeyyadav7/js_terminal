bbceb84b4c4dac4ffd0725ef509a5190172918df
Kartikey Yadav <kartikeyyadav.13579@gmail.com>
2022-01-10 23:42:10 +0530

    configuring snowpack

--- client/index.js
+++ client/index.js
@@ -1,11 +1,11 @@
 const serverAddress = "http://localhost:8080";
 
-// function connectToSocket(serverAddress) {
-// 	return new Promise((res) => {
-// 		const socket = io(serverAddress);
-// 		res(socket);
-// 	});
-// }
+function connectToSocket(serverAddress) {
+	return new Promise((res) => {
+		const socket = io(serverAddress);
+		res(socket);
+	});
+}
 
 function startTerminal(container, socket) {
 	const terminal = new TerminalUI(socket);
@@ -22,8 +22,15 @@ function start() {
 }
 
 // Better to start on DOMContentLoaded. So, we know terminal-container is loaded
-start();
+// start();
 
-// const term = new Terminal();
-// term.open(document.getElementById("terminal"));
-// term.write("Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ");
+const term = new Terminal();
+term.open(document.getElementById("terminal"));
+term.setOption("theme", {
+	background: "#202B33",
+	foreground: "#F5F8FA",
+});
+term.write("Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ");
+term.write("");
+term.write(`\r\n$ `);
+// Default text to display on terminal.
